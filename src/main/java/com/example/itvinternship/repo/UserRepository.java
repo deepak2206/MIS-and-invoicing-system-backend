37940342ce196f7e735a6ceb9fd6e4c9dc136dfa
deepak2206 <suthardeepak658@example.com>
2025-05-05 12:26:23 +0530

    first commit

--- src/main/java/com/example/itvinternship/repo/UserRepository.java
+++ src/main/java/com/example/itvinternship/repo/UserRepository.java
@@ -10,5 +10,5 @@ import com.example.itvinternship.model.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
-    Optional<User> findByResetToken(String resetToken);
+   
 }
